7fbd4432ca257b3bb32fa2f684c57a4120995aa6
Ravi <ravi.kiran8844@gmail.com>
2023-08-09 17:27:09 +0530

    first commit

---
+++ src/app/components/AnnouncmentBar/AnnouncmentBar.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const AnnouncmentBar = () => {
+  return (
+    <div className="bg-dark py-2">
+          <div className="container text-center">
+            <div className="main-text text-white">50% OFF on Making Charges on Gold Jewellery!</div>
+          </div>
+    </div>
+  )
+}
+
+export default AnnouncmentBar
