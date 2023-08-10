9cfcaa8ddd678fae9650edc976300ac6ddb5ca1b
Ravi <ravi.kiran8844@gmail.com>
2023-08-10 09:24:47 +0530

    updated slider

--- src/app/components/ImageSlider/ImageSlider.js
+++ src/app/components/ImageSlider/ImageSlider.js
@@ -8,20 +8,22 @@ import Image from 'next/image';
 
 const ImageSlider = () => {
   return (
-    <Splide aria-label="My Favorite Images">
-        <SplideSlide>
-                <Image loading='eager' priority  className='image-banner__desktop-img d-none d-md-block w-100 h-auto' width={1920} height={600} src="/main-slider-img1.webp" alt="Image Banner"/>
-                <Image loading='eager' priority  className='image-banner__mobile-img d-block d-md-none w-100 h-auto' width={428} height={600} src="/main-slider-mobile-img1.webp" alt="Image Banner"/>        
+   <div className='image-slider-section'>
+     <Splide aria-label="My Favorite Images">
+     <SplideSlide>
+                <Image loading='eager' priority  className='image-banner__desktop-img d-none d-md-block img-fluid' width={1920} height={740} src="/main-slider-img1.webp" alt="Image Banner"/>
+                <Image loading='eager' priority  className='image-banner__mobile-img d-block d-md-none img-fluid' width={428} height={579} src="/main-slider-mobile-img1.webp" alt="Image Banner"/>        
                 </SplideSlide>
                 <SplideSlide>
-                <Image loading='eager' priority  className='image-banner__desktop-img d-none d-md-block w-100 h-auto' width={1920} height={600} src="/main-slider-img1.webp" alt="Image Banner"/>
-                <Image loading='eager' priority  className='image-banner__mobile-img d-block d-md-none w-100 h-auto' width={428} height={600} src="/main-slider-mobile-img1.webp" alt="Image Banner"/>        
+                <Image loading='eager' priority  className='image-banner__desktop-img d-none d-md-block img-fluid' width={1920} height={740} src="/main-slider-img1.webp" alt="Image Banner"/>
+                <Image loading='eager' priority  className='image-banner__mobile-img d-block d-md-none img-fluid' width={428} height={579} src="/main-slider-mobile-img1.webp" alt="Image Banner"/>        
                 </SplideSlide>
                 <SplideSlide>
-                <Image loading='eager' priority  className='image-banner__desktop-img d-none d-md-block w-100 h-auto' width={1920} height={600} src="/main-slider-img1.webp" alt="Image Banner"/>
-                <Image loading='eager' priority  className='image-banner__mobile-img d-block d-md-none w-100 h-auto' width={428} height={600} src="/main-slider-mobile-img1.webp" alt="Image Banner"/>        
+                <Image loading='eager' priority  className='image-banner__desktop-img d-none d-md-block img-fluid' width={1920} height={740} src="/main-slider-img1.webp" alt="Image Banner"/>
+                <Image loading='eager' priority  className='image-banner__mobile-img d-block d-md-none img-fluid' width={428} height={579} src="/main-slider-mobile-img1.webp" alt="Image Banner"/>        
                 </SplideSlide>
         </Splide>
+   </div>
   )
 }
 
