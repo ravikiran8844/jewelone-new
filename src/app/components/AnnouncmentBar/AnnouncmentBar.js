6dd3e0e0e65189aa94cdc708ac1764dd0b4eaa5e
Ravi <ravi.kiran8844@gmail.com>
2023-08-10 11:17:11 +0530

    updated slider

--- src/app/components/AnnouncmentBar/AnnouncmentBar.js
+++ src/app/components/AnnouncmentBar/AnnouncmentBar.js
@@ -2,10 +2,8 @@ import React from 'react'
 
 const AnnouncmentBar = () => {
   return (
-    <div className="bg-dark py-2">
-          <div className="container text-center">
+    <div className="announcment-bar bg-dark">
             <div className="main-text text-white">50% OFF on Making Charges on Gold Jewellery!</div>
-          </div>
     </div>
   )
 }
--- src/app/components/NavBar/NavBar.js
+++ src/app/components/NavBar/NavBar.js
@@ -1,10 +1,55 @@
 import Image from 'next/image';
+import Link from 'next/link';
 import React from 'react';
 
 const NavBar = () => {
   return (
-    <div>
-        NavBar
+    <div className="main-header">
+      <div className="container">
+        <div className="header-1 col-12 d-flex justify-content-between align-items-center">
+          <div>
+            <div>
+              <Image className='img-fluid' height={66} width={256} src="/jewel-one-logo.png" alt="jewelone logo"/>
+            </div>
+          </div>
+          <div>
+            <div>
+              <ul className='list-unstyled d-flex justify-content-between m-auto'>
+                <li className='ms-2'>
+                  <Link href="/shop">
+                  <div className='text-center'>
+                  <Image className='img-fluid' height={32} width={32} src="/shop.svg" alt="shop"/>
+                    <div className='text-uppercase header1-menu-link-title  d-none d-md-block' >
+                    stores
+                    </div>
+                  </div>
+                  </Link>
+                </li>
+                <li className='ms-2'>
+                  <Link href="/account">
+                  <div className='text-center'>
+                  <Image className='img-fluid' height={32} width={32} src="/account.svg" alt="shop"/>
+                    <div className='text-uppercase header1-menu-link-title d-none d-md-block' >
+                    ACCOUNT
+                    </div>
+                  </div></Link>
+                </li>
+                <li className='ms-2'>
+                 <Link href="/cart">
+                 <div className='text-center'>
+                  <Image className='img-fluid' height={32} width={32} src="/cart.svg" alt="shop"/>
+
+                    <div className='text-uppercase header1-menu-link-title d-none d-md-block' >
+                    CART
+                    </div>
+                  </div>
+                 </Link>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
